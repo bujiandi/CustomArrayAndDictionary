7b793668236904a2251ea19a725739fdec4fed39
慧趣小歪 <huiquhome@163.com>
2015-08-05 17:29:06 +0800

    已更新

--- CustomArrayAndDictionary/OArray.swift
+++ CustomArrayAndDictionary/OArray.swift
@@ -1,14 +1,34 @@
 import Foundation
 
-class OArray<Element> : CollectionType, Indexable, SequenceType, MutableCollectionType, _DestructorSafeContainer, ArrayLiteralConvertible {
+class OArray<Element> : CollectionType, Indexable, SequenceType, MutableCollectionType, _DestructorSafeContainer {
     
+    typealias Index = Int
+    
+    private var _offset:Int = 0
     private var _count:Int = 0
     private var _capacity:Int = 10
     private var _minimumCapacity:Int = 10
     private var _pointer:UnsafeMutablePointer<Element>
     
+    /// ArrayLiteralConvertible
+    required init(arrayLiteral elements: Element...) {
+        _count = elements.count
+        _capacity = _count
+        _minimumCapacity = _capacity
+        _pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
+        for var i:Int = 0; i<_capacity; i++ {
+            _pointer.advancedBy(i).initialize(elements[advance(elements.startIndex, i)])
+        }
+    }
+    
+    init(capacity: Int) {
+        _capacity = capacity
+        _minimumCapacity = capacity
+        _pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
+    }
+    
     /// Construct an empty Array.
-    init() {
+    required init() {
         _pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
     }
     
@@ -32,12 +52,6 @@ class OArray<Element> : CollectionType, Indexable, SequenceType, MutableCollecti
         _count = count
     }
     
-    required init(capacity: Int) {
-        _capacity = capacity
-        _minimumCapacity = capacity
-        _pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
-    }
-    
     deinit {
         _pointer.destroy(_count)
         _pointer.dealloc(_capacity)
@@ -53,45 +67,36 @@ class OArray<Element> : CollectionType, Indexable, SequenceType, MutableCollecti
     
     subscript (index: Int) -> Element {
         get {
-            //            if index >= _count {
-            //                fatalError("index(\(index)) out of count(\(_count))")
-            //            }
-            return _pointer.advancedBy(index).memory
+//            if index >= _count {
+//                fatalError("index(\(index)) out of count(\(_count))")
+//            }
+            return _pointer.advancedBy(_offset + index).memory
         }
         set {
-            //            if index >= _count {
-            //                fatalError("index(\(index)) out of count(\(_count))")
-            //            }
-            _pointer.advancedBy(index).memory = newValue
+//            if index >= _count {
+//                fatalError("index(\(index)) out of count(\(_count))")
+//            }
+            _pointer.advancedBy(_offset + index).memory = newValue
         }
     }
     
     
     /// A type that can represent a sub-range of an `Array`.
-    //    typealias SubSlice = ArraySlice<Element>
-    //    subscript (subRange: Range<Int>) -> SubSlice {
-    //
-    //    }
-    required init(arrayLiteral elements: Element...) {
-        _capacity = elements.count
-        _minimumCapacity = _capacity
-        
-        _pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
-        for var i:Int = 0; i<_capacity; i++ {
-            _pointer.advancedBy(i).initialize(elements[elements.startIndex])
-        }
-    }
+//    typealias SubSlice = ArraySlice<Element>
+//    subscript (subRange: Range<Int>) -> SubSlice {
+//
+//    }
 }
 
 
-extension OArray  {
+extension OArray :  RangeReplaceableCollectionType, ArrayLiteralConvertible {
     
     
     /// The number of elements the Array stores.
     var count: Int { return _count }
     
     /// The number of elements the `Array` can store without reallocation.
-    var capacity: Int { return _capacity }
+    var capacity: Int { return _capacity - _offset }
     
     /// Reserve enough space to store `minimumCapacity` elements.
     ///
@@ -101,7 +106,7 @@ extension OArray  {
     /// - Complexity: O(`count`).
     func reserveCapacity(minimumCapacity: Int) {
         _minimumCapacity = minimumCapacity
-        if _capacity >= minimumCapacity && _count < _minimumCapacity {
+        if _capacity >= minimumCapacity && _count < minimumCapacity {
             resizeUnsafeCapacity(minimumCapacity)
         } else if _capacity < minimumCapacity {
             resizeUnsafeCapacity(minimumCapacity)
@@ -110,10 +115,11 @@ extension OArray  {
     
     private func resizeUnsafeCapacity(minimumCapacity: Int) {
         let pointer = UnsafeMutablePointer<Element>.alloc(minimumCapacity)
-        pointer.moveInitializeBackwardFrom(_pointer, count: _count)
+        pointer.moveInitializeBackwardFrom(_pointer.advancedBy(_offset), count: _count)
         _pointer.dealloc(_capacity)
         _pointer = pointer
         _capacity = minimumCapacity
+        _offset = 0
     }
     
     
@@ -121,10 +127,10 @@ extension OArray  {
     ///
     /// - Complexity: Amortized O(1) unless `self`'s storage is shared with another live array; O(`count`) if `self` does not wrap a bridged `NSArray`; otherwise the efficiency is unspecified..
     func append(newElement: Element) {
-        if _count == _capacity {
+        if (_offset + _count) == _capacity {
             resizeUnsafeCapacity(Int(Double(_count) * 1.6) + 1)
         }
-        _pointer.advancedBy(_count++).initialize(newElement)
+        _pointer.advancedBy(_offset + _count++).initialize(newElement)
     }
     
     /// Append the elements of `newElements` to `self`.
@@ -159,13 +165,39 @@ extension OArray  {
         return element
     }
     
+    func removeFirst() -> Element {
+        if _count-- == 0 {
+            fatalError("can't remove first because count is zero")
+        }
+        return _pointer.advancedBy(_offset++).move()
+    }
+    
     /// Insert `newElement` at index `i`.
     ///
     /// - Requires: `i <= count`.
     ///
     /// - Complexity: O(`count`).
     func insert(newElement: Element, atIndex i: Int) {
-        
+        if i > _count {
+            fatalError("can't insert because index out of range")
+        } else if i == 0 && _offset > 0 {
+            //print("使用普通缓冲")
+            _pointer.advancedBy(--_offset).initialize(newElement)
+             _count++
+            return
+        }
+        let oldCapacity = _capacity
+        if (_offset + _count) == _capacity {
+            _capacity = Int(Double(_count) * 1.6) + 1
+        }
+        let pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
+        if i > 0 { pointer.moveInitializeBackwardFrom(_pointer.advancedBy(_offset), count: i) }
+        pointer.advancedBy(i).initialize(newElement)
+        if i < _count { pointer.advancedBy(i + 1).moveInitializeBackwardFrom(_pointer.advancedBy(_offset + i), count: _count - i) }
+        _pointer.advancedBy(_offset).destroy(_count++)
+        _pointer.dealloc(oldCapacity)
+        _offset = 0
+        _pointer = pointer
     }
     
     /// Remove and return the element at index `i`.
@@ -174,7 +206,23 @@ extension OArray  {
     ///
     /// - Complexity: O(`count`).
     func removeAtIndex(index: Int) -> Element {
-        return _pointer.memory
+        if index >= _count {
+            fatalError("can't insert because index out of range")
+        } else if index == 0 {
+            return removeFirst()
+        } else if index == _count - 1 {
+            return removeLast()
+        }
+        let element = _pointer.advancedBy(_offset + index).memory
+        let pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
+        if index > 0 { pointer.moveInitializeBackwardFrom(_pointer.advancedBy(_offset), count: index) }
+        if index < _count - 1 { pointer.advancedBy(index).moveInitializeBackwardFrom(_pointer.advancedBy(_offset + index + 1), count: _count - index - 1) }
+        _pointer.advancedBy(_offset).destroy(_count--)
+        _pointer.dealloc(_capacity)
+        _offset = 0
+        _pointer = pointer
+
+        return element
     }
     
     /// Remove all elements.
@@ -189,6 +237,39 @@ extension OArray  {
             _capacity = _minimumCapacity
             _pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
         }
+    }
+    
+    func replaceRange<C : CollectionType where C.Generator.Element == Element>(subRange: Range<Int>, with newElements: C) {
+        if subRange.endIndex >= _count {
+            fatalError("can't replaceRange because subRange out of range")
+        }
+        var elements:Array<Element> = Array<Element>(newElements)
+        if elements.count == subRange.count {
+            for var i:Int = subRange.startIndex; i<subRange.endIndex; i++ {
+                _pointer.advancedBy(_offset + i).memory = elements[i - subRange.startIndex]
+            }
+        } else {
+            _capacity = _count + elements.count - subRange.count
+            print("_capacity:\(_capacity)")
+            let pointer = UnsafeMutablePointer<Element>.alloc(_capacity)
+            if subRange.startIndex > 0 { pointer.moveInitializeBackwardFrom(_pointer.advancedBy(_offset), count: subRange.startIndex) }
+            for var i:Int = subRange.startIndex; i<subRange.endIndex; i++ {
+                print("i:\(i) index:\(i - subRange.startIndex)")
+                pointer.advancedBy(i).initialize(elements[i - subRange.startIndex])
+            }
+            if subRange.endIndex < _count - 1 { pointer.advancedBy(subRange.endIndex).moveInitializeBackwardFrom(_pointer.advancedBy(_offset + subRange.endIndex + 1), count: _count - subRange.endIndex - 1) }
+            _count = _capacity
+            _offset = 0
+        }
+//        if newElements.count == subRange.count {
+//            
+//        }
+//        let length:Int = subRange.count
+//        let newLength:Int = distance(newElements.startIndex, newElements.endIndex)
+//        let length:Int = distance(newElements.startIndex, newElements.endIndex)
+//        if length == subRange.count {
+//            
+//        }
     }
     
     /// Interpose `self` between each consecutive pair of `elements`,
@@ -209,13 +290,24 @@ extension OArray  {
 
 extension OArray : CustomStringConvertible, CustomDebugStringConvertible {
     
+    func componentsJoinedByString(separator: String) -> String {
+        var result = ""
+        for var i:Int = _offset; i<_count + _offset; i++ {
+            if !result.isEmpty {
+                result += separator
+            }
+            result += "\(_pointer.advancedBy(i).memory)"
+        }
+        return result
+    }
     /// A textual representation of `self`.
     var description: String {
-        return ""
+        let content = componentsJoinedByString(", ")
+        return "[\(content)]"
     }
     
     /// A textual representation of `self`, suitable for debugging.
     var debugDescription: String {
-        return description
+        return "OArray<\(Element.self)>\(description)"
     }
 }
--- CustomArrayAndDictionary/main.swift
+++ CustomArrayAndDictionary/main.swift
@@ -7,10 +7,33 @@
 //
 
 import Foundation
-
+let str:String = "33"
 print("Hello, World!")
 
-let defaultArray:Array<Int> = []
+var defaultArray:Array<Int> = [1,2,6]
 
 let objArray:OArray<Int> = [1,3,5]
+defaultArray.insert(88, atIndex: 3)
+print(defaultArray)
 
+objArray.append(9)
+objArray.append(10)
+objArray.append(11)
+objArray.append(12)
+
+
+objArray.insert(77, atIndex: 3)
+objArray[3] = 22
+
+objArray.removeAtIndex(4)
+
+objArray.removeFirst()
+objArray.removeFirst()
+
+objArray.insert(99, atIndex: 0)
+objArray.insert(98, atIndex: 0)
+objArray.insert(97, atIndex: 0)
+
+objArray.replaceRange(1..<2, with: [9,3])
+
+print(objArray)
